059a219ba7ce93d94bf817e7622ef198a92acfb0
Vladimir Trajkovski <vl4d1m1r4@gmail.com>
2025-04-29 22:06:46 +0200

    Update text

--- app/routes/category-overview.$categoryId.tsx
+++ app/routes/category-overview.$categoryId.tsx
@@ -605,11 +605,11 @@ export default function CategoryOverview() {
         )}
         <div className="max-w-6xl mx-auto relative z-10 mt-8 text-center text-white">
           <p className="font-bold text-lg md:text-xl mb-2 [text-shadow:1px_1px_3px_rgba(0,0,0,0.7)]">
-            Tournament Opening SATURDAY 03.05.2025 at 18:00 in the sports hall
+            TOURNAMENT OPENING Saturday 03.05.2025 at 18:00 in the sports hall
             Dr. Jovan Kalauzi
           </p>
           <p className="font-bold text-lg md:text-xl [text-shadow:1px_1px_3px_rgba(0,0,0,0.7)]">
-            Отварање на турнирот САБОТА 03.05.2025г во 18:00 часот во спортска
+            ОТВОРАЊЕ НА ТУРНИРОТ сабота 03.05.2025г во 18:00 часот во спортска
             сала Д-р Јован Калаузи
           </p>
         </div>
